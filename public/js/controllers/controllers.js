943e617ad3b1ae4721836dc6cf2d7deb2783f6bb
Michael Sundell <msundell@shapestone.com>
2014-02-27 23:46:29 -0800

    Added Angular catalog controller

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -3,3 +3,38 @@
 /* Controllers */
 
 var apilaryControllers = angular.module('apilaryControllers', []);
+
+apilaryControllers.controller('CatalogController', ['$scope',
+  function($scope) {
+    var catalogs = [
+      {
+        "name":"ORG API",
+        "versions":[
+          {"version":"V0.8", "status":"retired"},
+          {"version":"V0.9", "status":"deprecated"},
+          {"version":"V1",   "status":"active"},
+          {"version":"V1.1", "status":"draft"}
+        ],
+        "description":"Provides APIs to organization information"
+      },
+      {
+        "name":"Catalog API",
+        "versions":[
+          {"version":"V1",   "status":"active"}
+        ],
+        "description":"Product catalog management API"
+      },
+      {
+        "name":"Search API",
+        "versions":[
+          {"version":"V1 beta",   "status":"draft"}
+        ],
+        "description":"Generic search API"
+      }
+    ];
+    $scope.catalogs = catalogs;
+
+    $scope.listCatalogs = function() {
+    }
+
+  }]);
