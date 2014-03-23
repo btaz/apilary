43c888a74327839ca29634dc04ed8e3f54af2b56
Michael Sundell <msundell@shapestone.com>
2014-03-22 23:06:20 -0700

    Additional setup

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,23 @@
 /* App Module */
 
 var apilaryApp = angular.module('apilaryApp', [
+  'ngRoute',
   'apilaryControllers',
   'apilaryServices'
 ]);
+
+apilaryApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/catalogs', {
+        templateUrl: 'partials/catalog-list.html',
+        controller: 'CatalogListController'
+      }).
+      when('/catalog/:catalogId', {
+        templateUrl: 'partials/catalog-detail.html',
+        controller: 'CatalogDetailController'
+      }).
+      otherwise({
+        redirectTo: '/catalogs'
+      });
+  }]);
--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -4,10 +4,11 @@
 
 var apilaryControllers = angular.module('apilaryControllers', []);
 
-apilaryControllers.controller('CatalogController', ['$scope',
+apilaryControllers.controller('CatalogListController', ['$scope',
   function($scope) {
     var catalogs = [
       {
+        "id":"1000",
         "name":"ORG API",
         "versions":[
           {"version":"V0.8", "status":"retired"},
@@ -18,6 +19,7 @@ apilaryControllers.controller('CatalogController', ['$scope',
         "description":"Provides APIs to organization information"
       },
       {
+        "id":"1001",
         "name":"Catalog API",
         "versions":[
           {"version":"V1",   "status":"active"}
@@ -25,6 +27,7 @@ apilaryControllers.controller('CatalogController', ['$scope',
         "description":"Product catalog management API"
       },
       {
+        "id":"1002",
         "name":"Search API",
         "versions":[
           {"version":"V1 beta",   "status":"draft"}
@@ -35,6 +38,53 @@ apilaryControllers.controller('CatalogController', ['$scope',
     $scope.catalogs = catalogs;
 
     $scope.listCatalogs = function() {
+    }
+
+  }]);
+
+apilaryControllers.controller('CatalogDetailController', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    var catalogId = $routeParams.catalogId;
+
+    var catalogs = [
+      {
+        "id":"1000",
+        "name":"ORG API",
+        "versions":[
+          {"version":"V0.8", "status":"retired"},
+          {"version":"V0.9", "status":"deprecated"},
+          {"version":"V1",   "status":"active"},
+          {"version":"V1.1", "status":"draft"}
+        ],
+        "description":"Provides APIs to organization information"
+      },
+      {
+        "id":"1001",
+        "name":"Catalog API",
+        "versions":[
+          {"version":"V1",   "status":"active"}
+        ],
+        "description":"Product catalog management API"
+      },
+      {
+        "id":"1002",
+        "name":"Search API",
+        "versions":[
+          {"version":"V1 beta",   "status":"draft"}
+        ],
+        "description":"Generic search API"
+      }
+    ];
+    $scope.catalog = getCatalog(catalogs, catalogId);
+    $scope.catalogs = catalogs;
+
+    function getCatalog(catalogs, catalogId) {
+      for(var i=0; i<catalogs.length; i++) {
+        if(catalogs[i].id == catalogId) {
+          return catalogs[i];
+        }
+      }
+      // throw error here
     }
 
   }]);
